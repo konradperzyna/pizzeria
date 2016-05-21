3b5d2473ac474c73aad879392db62225ee30bc6a
Konrad Perzyna <konrad@perzyna.net>
2016-05-21 16:58:28 +0200

    Automatic injection of js files into index.html.

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -2,12 +2,20 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
     del = require('del'),
+    inject = require('gulp-inject'),
+    es = require('event-stream'),
+    //streams = require('stream-series'),
+    //angularFilesort = require('gulp-angular-filesort'),
+    //browserify = require('browserify'),
+    //source = require('vinyl-source-stream'),
+    ngAnnotate = require('gulp-ng-annotate'),
     jshint = require('gulp-jshint');
 
 
 gulp.task('jshint', function() {
-    gulp.src(['*.js', 'js/*.js', 'data/*.js', 'lib/*.js'])
+    gulp.src(['./**/*.js', '!node_modules/**/*'])
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         //.pipe(jshint.reporter('fail'))
@@ -15,8 +23,8 @@ gulp.task('jshint', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['*.js', 'js/*.js', 'data/*.js', 'lib/.*js', '!gulpfile.js', '*.html', 'partials/*.html', 'css/*.css'], copy_dev)
-})
+    gulp.watch(['./**/*.js', '!node_modules/**/*', '!gulpfile.js', './**/*.html', 'css/*.css'], copy_dev)
+});
 
 gulp.task('clean', function () {
   return del([
@@ -26,26 +34,35 @@ gulp.task('clean', function () {
 
 
 function copy_dev() {
-    gulp.src(['node_modules/angular/angular.js', 'node_modules/angular-ui-router/release/angular-ui-router.js']).pipe(gulp.dest('../build/lib/'));
-    gulp.src(['*.js', '!gulpfile.js']).pipe(gulp.dest('../build/'));
-    gulp.src('js/*.js').pipe(gulp.dest('../build/js/'));
-    gulp.src('data/*.js').pipe(gulp.dest('../build/data/'));
-    gulp.src('*.html').pipe(gulp.dest('../build/'));
-    gulp.src('partials/*.html').pipe(gulp.dest('../build/partials/'));
-    gulp.src('css/*.css').pipe(gulp.dest('../build/css/'));
-
+     var angularStream = gulp.src(['node_modules/angular/angular.js', 'node_modules/angular-ui-router/release/angular-ui-router.js']).pipe(gulp.dest('../build/lib/'));
+     var jsStream = gulp.src(['./**/*.js', '!node_modules/**/*', '!gulpfile.js']).pipe(gulp.dest('../build/'));
+     var cssStream = gulp.src('css/*.css').pipe(gulp.dest('../build/css/'));
+     gulp.src(['./**/*.html', '!index.html', '!node_modules/**/*']).pipe(gulp.dest('../build/'));
+     gulp.src('index.html')
+         .pipe(inject(es.merge([angularStream, jsStream, cssStream]), {ignorePath: '../build/'}))
+         .pipe(gulp.dest('../build/'));
 }
-
 
 gulp.task('build:dev', ['clean'], copy_dev);
 
 
 gulp.task('build:prod', ['clean'], function() {
-    gulp.src(['*.js', 'js/*.js', 'data/*.js', '!gulpfile.js', 'node_modules/angular/angular.js', 'node_modules/angular-ui-router/release/angular-ui-router.js'])
+    var jsStream = gulp.src([ 'node_modules/angular/angular.js', 'node_modules/angular-ui-router/release/angular-ui-router.js', 
+                                './**/*.js', '!gulpfile.js', '!node_modules/**/*'])
+        .pipe(sourcemaps.init())
         .pipe(concat('pizzeria.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('../build/'));
-    gulp.src('*.html').pipe(gulp.dest('../build/'));
-    gulp.src('partials/*.html').pipe(gulp.dest('../build/partials/'));
-    gulp.src('css/*.css').pipe(gulp.dest('../build/css/'));
+        // //TODO: uglify destroys angular injection system?
+        // .pipe(ngAnnotate(
+        //     {
+        //     // true helps add where @ngInject is not used. It infers.
+        //     // Doesn't work with resolve, so we must be explicit there
+        //     add: true
+        //     }
+        // ))        
+    	// .pipe(uglify())
+        .pipe(sourcemaps.write('../build/'))
+        .pipe(gulp.dest('../build/'));
+    var cssStream = gulp.src('css/*.css').pipe(gulp.dest('../build/css/'));
+    gulp.src(['./**/*.html', '!index.html', '!node_modules/**/*']).pipe(gulp.dest('../build/'));
+    gulp.src('index.html').pipe(inject(es.merge(jsStream, cssStream), {ignorePath: '../build/'})).pipe(gulp.dest('../build/'));
 });
--- server/js/app.js
+++ server/js/app.js
@@ -1,4 +1,4 @@
-angular.module('pizzeria', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+angular.module('pizzeria', ['ui.router']).config(/* @ngInject */ function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('main', {
             url: '/main',
